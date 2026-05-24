3594464908b2bdae241c06b1b831fee49cc002c8
Aahana Surya <surya.a@northeastern.edu>
2026-05-24 00:04:14 -0700

    qr code generate made aesthetic and html page added

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
+const form = document.getElementById('qr-form');
+const qrOutput = document.getElementById('qr-output');
+const qrContainer = document.getElementById('qrcode');
 
-import inquirer from 'inquirer';
-import qr from "qr-image";
-import fs from "fs";
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const url = document.getElementById('url-input').value.trim();
 
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {message: "Give me your url: ",
-    name: "URL",},
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    const url = answers.URL;
-    var qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+  if (!url) {
+    return;
+  }
 
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
+  if (typeof QRCode === 'undefined') {
+    alert('Unable to load the QR code library. Please refresh the page and try again.');
+    return;
+  }
+
+  qrContainer.innerHTML = '';
+  new QRCode(qrContainer, {
+    text: url,
+    width: 240,
+    height: 240,
+    colorDark: '#111827',
+    colorLight: '#ffffff',
+    correctLevel: QRCode.CorrectLevel.H,
   });
+
+  qrOutput.hidden = false;
+});
   
- 
 
 
 
